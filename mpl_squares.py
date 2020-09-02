0f923710ab898d1fdab34d9f119635ffe54339ce
FloppyDogs <57467298+FloppyDogs@users.noreply.github.com>
2020-09-02 11:36:50 -0700

    Initial commit

---
+++ mpl_squares.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 11:13:30 2020
+
+@author: Jake
+"""
+
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+
+plt.style.use('fivethirtyeight')
+
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth = 3)
+
+#Set chart titles and labels 
+ax.set_title("Square Numbers", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square of Value", fontsize = 14)
+
+#Set tick of labels 
+ax.tick_params(axis = 'both', labelsize = 14)
+plt.show()
